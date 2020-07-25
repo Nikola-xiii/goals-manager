e4c7f58183bb02ab9e862c6d2828db9f54973e11
nikola-xiii <nikola.serdiuk@gmail.com>
2020-07-25 19:28:36 +0300

    #2: Connected form to redux

--- src/features/goal/goal-form/goal-form.tsx
+++ src/features/goal/goal-form/goal-form.tsx
@@ -1,40 +1,55 @@
 import React from 'react';
 import {Button, Card, Form} from "react-bootstrap";
+import {Goal} from "../../../store/models/goal.model";
 
-const GoalForm = () => {
-  return(
-    <Card>
-      <Card.Body>
-        <Card.Title>Add New OKR Goal</Card.Title>
-        <Form>
-          <Form.Group controlId="formObjectives">
-            <Form.Label>Objectives</Form.Label>
-            <Form.Control type="text" placeholder="Enter objective" />
-            <Form.Text className="text-muted">
-              What are some things you want to accomplish this sprint?
-            </Form.Text>
-          </Form.Group>
-          <Form.Group controlId="formKey">
-            <Form.Label>Key</Form.Label>
-            <Form.Control type="text" placeholder="Enter key" />
-            <Form.Text className="text-muted">
-              What is the key to measure this goal?
-            </Form.Text>
-          </Form.Group>
-          <Form.Group controlId="formResults">
-            <Form.Label>Result</Form.Label>
-            <Form.Control as="textarea" placeholder="Enter result" />
-            <Form.Text className="text-muted">
-              What is the result to measure this goal?
-            </Form.Text>
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Add Goal
-          </Button>
-        </Form>
-      </Card.Body>
-    </Card>
-  );
+interface propsType {
+  form: { goalForm: Goal },
+  initGoal: (form: Goal) => {},
+  updateGoal: (form: Goal) => {},
+  createGoal: (form: Goal) => {},
+  deleteGoal: (form: Goal) => {},
+}
+
+class GoalForm extends React.Component<propsType> {
+  handleChange = (event) => {
+    this.setState({value: event.target.value});
+  }
+  render() {
+    return(
+      <Card>
+        <Card.Body>
+          <Card.Title>Add New OKR Goal</Card.Title>
+          {console.log(this.props)}
+          <Form>
+            <Form.Group controlId="formObjectives">
+              <Form.Label>Objectives</Form.Label>
+              <Form.Control type="text" placeholder="Enter objective" value={this.props.form.goalForm.objective} />
+              <Form.Text className="text-muted">
+                What are some things you want to accomplish this sprint?
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formKey">
+              <Form.Label>Key</Form.Label>
+              <Form.Control type="text" placeholder="Enter key" value={this.props.form.goalForm.key} />
+              <Form.Text className="text-muted">
+                What is the key to measure this goal?
+              </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formResults">
+              <Form.Label>Result</Form.Label>
+              <Form.Control as="textarea" placeholder="Enter result" value={this.props.form.goalForm.results} />
+              <Form.Text className="text-muted">
+                What is the result to measure this goal?
+              </Form.Text>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Add Goal
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    );
+  }
 }
 
 export default GoalForm;
--- src/features/goal/new-goal/new-goal.tsx
+++ src/features/goal/new-goal/new-goal.tsx
@@ -1,8 +1,18 @@
 import GoalForm from "../goal-form/goal-form";
 import React from "react";
+import {createGoalForm, initGoalForm, removeGoalForm, updateGoalForm} from "../../../store/actions";
+import {Goal} from "../../../store/models/goal.model";
+import {connect} from "react-redux";
 
-function NewGoal() {
-  return (<GoalForm />);
-}
+const mapStateToProps = (state: any) => ({
+  form: state.goalForm
+});
 
-export default NewGoal;
+const mapDispatchToProps = (dispatch: any) => ({
+  initGoal: (form: Goal) => dispatch(initGoalForm(form)),
+  updateGoal: (form: Goal) => dispatch(updateGoalForm(form)),
+  createGoal: (form: Goal) => dispatch(createGoalForm(form)),
+  deleteGoal: (form: Goal) => dispatch(removeGoalForm(form)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(GoalForm);
