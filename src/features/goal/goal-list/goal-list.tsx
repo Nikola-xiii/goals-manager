da37506132efaf251b7d57f82c56d097e924387b
nikola-xiii <nikola.serdiuk@gmail.com>
2020-07-27 23:15:47 +0300

    #3: List connect to store

--- src/features/goal/goal-list/goal-list.tsx
+++ src/features/goal/goal-list/goal-list.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import GoalListItem from "../goal-list-item/goal-list-item";
 import {Goal} from "../../../store/models/goal.model";
+import {connect} from "react-redux";
+import {getGoalList} from "../../../store/actions";
 
 const Goals: Goal[] = [
   {
@@ -13,12 +15,24 @@ const Goals: Goal[] = [
     key: 'Workout',
     results: 'Do workout 2 time per week'
   }
-]
+];
+
+const mapStateToProps = (state: any) => ({
+  goals: state.goals.data
+});
+
+const mapDispatchToProps = (dispatch: any) => ({
+  initGoalList: (goals: Goal[]) => dispatch(getGoalList(goals)),
+});
 
 class GoalList extends React.Component<any, any>{
+  componentDidMount() {
+    this.props.initGoalList(Goals);
+  }
+
   render() {
-    return Goals.map(goal => (<GoalListItem goal={goal}></GoalListItem>));
+    return this.props.goals ? this.props.goals.map((goal: Goal) => (<GoalListItem goal={goal}></GoalListItem>)) : <div>Goals not found</div> ;
   }
 }
 
-export default GoalList;
+export default connect(mapStateToProps, mapDispatchToProps)(GoalList);
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,4 +1,4 @@
-import {INIT_GOAL_FORM} from "./actions";
+import {CREATE_GOAL, GOAL_LIST, INIT_GOAL_FORM} from "./actions";
 import {combineReducers} from "redux";
 import {Goal} from "./models/goal.model";
 
@@ -10,7 +10,7 @@ const initialGoalState = {
   }
 }
 
-function goalReducer(state = initialGoalState, action: { type: string; form: Goal; }) {
+function goalReducer(state = initialGoalState, action: { type: string; form: Goal; goals: Goal[]}) {
   switch (action.type) {
     case INIT_GOAL_FORM:
       return {
@@ -18,11 +18,31 @@ function goalReducer(state = initialGoalState, action: { type: string; form: Goa
         status: INIT_GOAL_FORM,
         data: action.form,
       };
+    case CREATE_GOAL:
+      return {
+        ...state,
+        status: CREATE_GOAL,
+        data: action.form,
+      };
     default:
       return state;
   }
 }
 
+function goalListReducer(state: Goal[] = [], action: { type: string; goals: Goal[]}) {
+  switch (action.type) {
+    case GOAL_LIST:
+      return {
+        ...state,
+        status: GOAL_LIST,
+        data: action.goals,
+      };
+    default:
+      return state;
+  }
+};
+
 export default combineReducers({
-  goalForm: goalReducer
+  goalForm: goalReducer,
+  goals: goalListReducer
 })
