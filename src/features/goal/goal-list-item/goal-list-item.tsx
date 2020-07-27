da78cc699d17abc7b97928a7d06e313bce1ac830
nikola-xiii <nikola.serdiuk@gmail.com>
2020-07-27 09:28:08 +0300

    #3: Render item in list

--- src/features/goal/goal-list-item/goal-list-item.tsx
+++ src/features/goal/goal-list-item/goal-list-item.tsx
@@ -1,13 +1,18 @@
 import {Card} from "react-bootstrap";
 import * as React from "react";
+import './goal-list-item.scss';
+import {Goal} from "../../../store/models/goal.model";
 
-const GoalListItem = () => {
+const GoalListItem = (props: { goal: Goal }) => {
   return (
     <Card>
+      <Card.Header>
+        <span className="label">Objective</span>
+        <h2>{props.goal.objective}</h2>
+      </Card.Header>
       <Card.Body>
-        <p><strong>Objective</strong>: Future job</p>
-        <p><strong>Key</strong>: Future job</p>
-        <p><strong>Results</strong>: Prepare for the interview</p>
+        <p><strong>Key</strong>: {props.goal.key}</p>
+        <p><strong>Results</strong>: {props.goal.results}</p>
       </Card.Body>
     </Card>
   )
--- src/features/goal/goal-list/goal-list.tsx
+++ src/features/goal/goal-list/goal-list.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import GoalListItem from "../goal-list-item/goal-list-item";
+import {Goal} from "../../../store/models/goal.model";
+
+const Goals: Goal[] = [
+  {
+    objective: 'Get job in Wix',
+    key: 'Prepare for Interview',
+    results: 'Pluralsight IQ - Expert'
+  },
+  {
+    objective: 'Body Health',
+    key: 'Workout',
+    results: 'Do workout 2 time per week'
+  }
+]
 
 class GoalList extends React.Component<any, any>{
   render() {
-    return (
-      <GoalListItem></GoalListItem>
-    );
+    return Goals.map(goal => (<GoalListItem goal={goal}></GoalListItem>));
   }
 }
 
